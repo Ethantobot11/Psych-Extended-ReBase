b22d44a9098a9bc0d8a4cb6cedf1d14a50d61e81
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2026-01-23 21:22:22 +0100

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -7,7 +7,7 @@ echo Makking the main haxelib and setuping folder in same time..
 haxelib setup ~/haxelib
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
-haxelib git linc_luajit https://github.com/PsychExtendedThings/linc_luajit --quiet #Sirox's linc_luajit (backup)
+haxelib git linc_luajit https://github.com/th2l-devs/linc_luajit --quiet #Sirox's linc_luajit (backup)
 haxelib git flixel https://github.com/PsychExtendedThings/flixel 5.6.1 --quiet
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc --quiet
 haxelib install flixel-addons 3.2.2 --quiet
@@ -17,8 +17,8 @@ haxelib git hscript-improved https://github.com/PsychExtendedThings/hscript-impr
 haxelib install hscript 2.4.0 --quiet
 haxelib install tjson 1.4.0 --quiet #for fucking mods system (haxe.Json brokes the modlist)
 haxelib install hxvlc 2.1.0 --quiet #That clearly my repo
-haxelib git hxcpp https://github.com/beihu235/hxcpp --quiet #Just a normal hxcpp
-haxelib git lime https://github.com/ArkoseLabsOfficial/lime --quiet #repo owner helped to fix termux problem
+haxelib git hxcpp https://github.com/AlafandyPorting/hxcpp --quiet #Just a normal hxcpp
+haxelib git https://github.com/ArkoseLabsOfficial/lime-termux --quiet #repo owner helped to fix termux problem
 haxelib install openfl 9.3.3 --quiet #Most stable version of openfl
 haxelib git flxanimate https://github.com/PsychExtendedThings/flxanimate-ex --quiet
 haxelib git funkin.vis https://github.com/beihu235/funkVis-FrequencyFixed main --quiet
