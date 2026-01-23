3e44ec29f2bb727978299e21b05ecbbd190b62c6
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2026-01-23 21:44:30 +0100

    Add files via upload

--- library/hxcodec/flixel/FlxVideo.hx
+++ library/hxcodec/flixel/FlxVideo.hx
@@ -1,155 +1,70 @@
-package hxcodec.flixel;
+package hxvlc.flixel;
 
+#if flixel
 import flixel.FlxG;
-import openfl.Lib;
-import openfl.events.Event;
-import sys.FileSystem;
-import hxcodec.flixel.VLCBitmap; //custom VLCBitmap
+import flixel.util.FlxAxes;
 
 /**
- * A custom MP4Handler class for hxvlc support.
- * @Modifier KralOyuncu 2010x (ArkoseLabs)
+ * This class extends `FlxInternalVideo` to display video files in HaxeFlixel.
+ *
+ * ```haxe
+ * final video:FlxVideo = new FlxVideo();
+ * video.onEndReached.add(function():Void
+ * {
+ * 	video.dispose();
+ *
+ * 	FlxG.removeChild(video);
+ * });
+ * FlxG.addChildBelowMouse(video);
+ *
+ * if (video.load('assets/videos/video.mp4'))
+ * 	FlxTimer.wait(0.001, () -> video.play());
+ * ```
  */
-class FlxVideo extends VLCBitmap
+class FlxVideo extends FlxInternalVideo
 {
-	public var canSkip:Bool = true;
-	public var canUseSound:Bool = true;
-	public var canUseAutoResize:Bool = true;
+	/** Determines the resizing behavior for the video. */
+	public var resizeMode(default, set):FlxAxes = FlxAxes.XY;
 
-	public var openingCallback:Void->Void = null;
-	public var finishCallback:Void->Void = null;
-
-	private var pauseMusic:Bool = false;
-
-	public function new(IndexModifier:Int = 0):Void
+	@:inheritDoc(hxvlc.openfl.Video.new)
+	public function new(smoothing:Bool = true):Void
 	{
-		super();
+		super(smoothing);
 
-		onOpening = onVLCVideoReady;
-		onEncounteredError = onVLCError;
-
-		FlxG.addChildBelowMouse(this, IndexModifier);
-	}
-
-	private function onVLCVideoReady():Void 
-	{
-		trace("the video is opening!");
-		if (openingCallback != null)
-			openingCallback();
-	}
-
-	private function onVLCError():Void
-	{
-		Lib.application.window.alert('Error cannot be specified', "VLC Error!");
-		killVideo();
-	}
-
-	//call function from here for double check
-	public override function stop() {
-		super.stop();
-	}
-
-	public override function pause() {
-		super.pause();
-	}
-
-	public override function resume() {
-		super.resume();
-	}
-
-	public function killVideo() {
-		stop();
-		dispose();
-	}
-
-	override public function dispose():Void
-	{
-		if (FlxG.autoPause)
+		onFormatSetup.add(function():Void
 		{
-			if (FlxG.signals.focusGained.has(resume))
-				FlxG.signals.focusGained.remove(resume);
+			if (!FlxG.signals.gameResized.has(onGameResized))
+				FlxG.signals.gameResized.add(onGameResized);
 
-			if (FlxG.signals.focusLost.has(pause))
-				FlxG.signals.focusLost.remove(pause);
-		}
+			onGameResized(FlxG.stage.stageWidth, FlxG.stage.stageHeight);
+		});
+	}
 
-		if (FlxG.stage.hasEventListener(Event.ENTER_FRAME))
-			FlxG.stage.removeEventListener(Event.ENTER_FRAME, onEnterFrame);
+	@:inheritDoc(hxvlc.openfl.Video.dispose)
+	public override function dispose():Void
+	{
+		if (FlxG.signals.gameResized.has(onGameResized))
+			FlxG.signals.gameResized.remove(onGameResized);
 
 		super.dispose();
-
-		FlxG.removeChild(this);
 	}
 
-	/**
-	 * Plays a video.
-	 *
-	 * @param Path Example: `your/video/here.mp4`
-	 * @param Loop Loop the video.
-	 * @param PauseMusic Pause music until the video ends.
-	 */
-	override public function load(?location:String = null, repeat:Int = 0):Bool
+	@:noCompletion
+	private function onGameResized(width:Int, height:Int):Void
 	{
-		if (FlxG.sound.music != null && pauseMusic)
-			FlxG.sound.music.pause();
-
-		// Probably won't help with anything but ok.
-		volume = Std.int(#if FLX_SOUND_SYSTEM ((FlxG.sound.muted || !canUseSound) ? 0 : 1) * #end FlxG.sound.volume * 100);
-
-		FlxG.stage.addEventListener(Event.ENTER_FRAME, update);
-
-		if (FlxG.autoPause)
+		if ((resizeMode.x || resizeMode.y) && bitmapData != null)
 		{
-			FlxG.signals.focusGained.add(resume);
-			FlxG.signals.focusLost.add(pause);
+			this.width = resizeMode.x ? FlxG.scaleMode.gameSize.x : bitmapData.width;
+			this.height = resizeMode.y ? FlxG.scaleMode.gameSize.y : bitmapData.height;
 		}
-
-		// in case if you want to use another dir then the application one.
-		// android can already do this, it can't use application's storage.
-		if (FileSystem.exists(Sys.getCwd() + location))
-			return super.load(Sys.getCwd() + location, repeat);
-		else
-			return super.load(location, repeat);
 	}
 
-	private function update(?E:Event):Void
+	@:noCompletion
+	private function set_resizeMode(value:FlxAxes):FlxAxes
 	{
-		#if FLX_KEYBOARD
-		if (canSkip && (FlxG.keys.justPressed.SPACE #if android || FlxG.android.justReleased.BACK #end) && (isPlaying && isDisplaying))
-			killVideo();
-		#elseif android
-		if (canSkip && FlxG.android.justReleased.BACK && (isPlaying && isDisplaying))
-			killVideo();
-		#end
+		onGameResized(FlxG.stage.stageWidth, FlxG.stage.stageHeight);
 
-		if (canUseAutoResize && (videoWidth > 0 && videoHeight > 0))
-		{
-			width = calcSize(0);
-			height = calcSize(1);
-		}
-
-		volume = Std.int(#if FLX_SOUND_SYSTEM ((FlxG.sound.muted || !canUseSound) ? 0 : 1) * #end FlxG.sound.volume * 100);
-	}
-
-	public function calcSize(Ind:Int):Int
-	{
-		var appliedWidth:Float = Lib.current.stage.stageHeight * (FlxG.width / FlxG.height);
-		var appliedHeight:Float = Lib.current.stage.stageWidth * (FlxG.height / FlxG.width);
-
-		if (appliedHeight > Lib.current.stage.stageHeight)
-			appliedHeight = Lib.current.stage.stageHeight;
-
-		if (appliedWidth > Lib.current.stage.stageWidth)
-			appliedWidth = Lib.current.stage.stageWidth;
-
-		switch (Ind)
-		{
-			case 0:
-				return Std.int(appliedWidth);
-			case 1:
-				return Std.int(appliedHeight);
-		}
-
-		return 0;
+		return resizeMode = value;
 	}
 }
+#end
