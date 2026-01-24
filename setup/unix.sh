cb7ee52590aa1caac6e6095ed92c1ff3586d84d3
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2026-01-24 01:41:01 +0100

    i hate my life

--- setup/unix.sh
+++ setup/unix.sh
@@ -23,6 +23,4 @@ haxelib install openfl 9.3.3 --quiet #Most stable version of openfl
 haxelib git flxanimate https://github.com/PsychExtendedThings/flxanimate-ex --quiet
 haxelib git funkin.vis https://github.com/beihu235/funkVis-FrequencyFixed main --quiet
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git 57f5d47f2533fd0c3dcd025a86cb86c0dfa0b6d2 --quiet
-echo "Installing Android-only libraries..."
-haxelib git extension-androidtools https://github.com/MAJigsaw77/extension-androidtools --quiet
 echo Finished!
